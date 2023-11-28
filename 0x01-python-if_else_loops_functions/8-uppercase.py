c85b822069846e4664c1070679ec9bccd7af0263
ahmed <ahmedissaten@gmail.com>
2023-11-28 15:06:59 +0200

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        a = ord(i)
+        if a > 96:
+            t = chr(a - 32)
+        else:
+            t = i
+            print("{}".format(t), end="")
+    print()
