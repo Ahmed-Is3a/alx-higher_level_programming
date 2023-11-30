a9e7dd8f75b4c07202bf7bf2c534ec36df0f6b1c
ahmed <ahmedissaten@gmail.com>
2023-11-30 22:41:34 +0200

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+if __name__ == "__main__":
 
-def add_arguments(arguments):
-    result = sum(int(arg) for arg in arguments)
-    return result
+    def add_arguments(arguments):
+        result = sum(int(arg) for arg in arguments)
+        return result
 
-
-arguments = sys.argv[1:]
-total = add_arguments(arguments)
-print(total)
+    arguments = sys.argv[1:]
+    total = add_arguments(arguments)
+    print(total)
