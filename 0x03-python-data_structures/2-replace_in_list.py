3db0b2be58fdb142e15479ec8e6aa7e2a5e7d960
ahmed <ahmedissaten@gmail.com>
2023-12-02 15:02:35 +0200

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    pass
+
+
+def replace_in_list(my_list, idx, element):
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = element
+
+    return my_list
