eac3e93f37d83a93cabf01a2a09a69fa210333fc
ahmed <ahmedissaten@gmail.com>
2023-12-02 15:57:56 +0200

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    pass
+
+def no_c(my_string):
+    new_str = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_str += i
+    return new_str
