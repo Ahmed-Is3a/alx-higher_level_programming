5896595de96149b63ef388fbb06776814bf81307
ahmed <ahmedissaten@gmail.com>
2023-12-04 13:37:29 +0200

    1-element_at.py fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    pass
+
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    return my_list(idx)
