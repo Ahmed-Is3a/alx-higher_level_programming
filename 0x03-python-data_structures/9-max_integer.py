a1b4d50842b04f7398197de111288d64dca7e09d
ahmed <ahmedissaten@gmail.com>
2023-12-04 14:49:31 +0200

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    pass
+
+
+def max_integer(my_list=[]):
+    max_int = 0
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if i > max_int:
+            max_int = i
+    return max_int
