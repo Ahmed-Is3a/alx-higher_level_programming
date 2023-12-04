59b909a9fe3f02838cbf57baac918956bb2b6288
ahmed <ahmedissaten@gmail.com>
2023-12-05 00:04:48 +0200

    7-add_tuple.py fic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    pass
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    # check if tuple has two element
+    # the add 0 to make the missing elements 0
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
+
+    result = (a[0] + b[0], a[1] + b[1])
+
+    return result
