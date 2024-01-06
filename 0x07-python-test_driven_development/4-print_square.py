36169216d3dfb819260014fcbaf6187cd195235d
ahmed <ahmedissaten@gmail.com>
2024-01-06 13:27:07 +0200

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def print_square(size):
+    """
+    print a square
+
+    Parametres:
+    size (int): the size of the squae
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
