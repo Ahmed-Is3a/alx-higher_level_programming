26b9f27100f550865ce7605b921d58721ba3cfb2
ahmed <ahmedissaten@gmail.com>
2024-01-08 22:10:38 +0200

    0-lookup.py fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
     """
@@ -12,4 +14,4 @@ def lookup(obj):
 
     """
 
-    return [att for att in (dir(obj))]
+    return (dir(obj))
