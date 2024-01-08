b67e99a6f9b50ccc5e5c2e7ff43b758cd585f8d8
ahmed <ahmedissaten@gmail.com>
2024-01-08 22:21:53 +0200

    3-is_kind_of_class.py bug fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines a class and inherited class-checking function."""
+
 
 def is_kind_of_class(obj, a_class):
     """
@@ -10,5 +12,10 @@ def is_kind_of_class(obj, a_class):
     obj (obj): object
     a_class (class):  a class
 
+    Returns:
+    True or False
     """
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
