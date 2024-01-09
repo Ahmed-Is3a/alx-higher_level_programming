adafa0fbe048d697a1a6e62c8ef33c48a0eaf0ca
ahmed <ahmedissaten@gmail.com>
2024-01-09 09:42:44 +0200

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Write a function that reads a text file (UTF8) and prints it to stdout:"""
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints it to stdout:
+
+    You must use the with statement
+    You don’t need to manage file permission or file doesn't exist exceptions.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)
