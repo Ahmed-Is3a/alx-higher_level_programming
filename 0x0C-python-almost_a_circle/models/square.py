6ca5686805e96c87ca1cc410dd2166dbc2888bc7
ahmed <ahmedissaten@gmail.com>
2024-01-15 09:44:27 +0200

    11 square class adding setter and getter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,11 +5,25 @@ from rectangle import Rectangle
 class Square(Rectangle):
     """ represent Square class that inherits from Rectangle """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
         self.size = size
         self.x = x
         self.y = y
         self.id = id
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ size getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ size setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = value
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
