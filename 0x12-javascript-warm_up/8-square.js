39809dee8c0f68d6057fcbb239e8a82765c03abe
ahmed <ahmedissaten@gmail.com>
2024-03-11 17:37:22 +0200

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+if (isNaN(arg)) {
+  console.log('Missing size');
+} else {
+  const size = parseInt(arg);
+  if (size <= 0) {
+    console.log('Invalid size');
+  } else {
+    for (let i = 0; i < size; i++) {
+      let row = '';
+      for (let j = 0; j < size; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
