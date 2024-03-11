d77883e844efd16206807be5daf1b87e5915eb14
ahmed <ahmedissaten@gmail.com>
2024-03-11 23:10:57 +0200

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2).map(Number);
-
-if (args.length === 0 || args.length === 1) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  let max = args[0];
-  let secondMax = args[0];
-
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] > max) {
-      secondMax = max;
-      max = args[i];
-    } else if (args[i] > secondMax && args[i] < max) {
-      secondMax = args[i];
-    }
-  }
-
-  console.log(secondMax);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
