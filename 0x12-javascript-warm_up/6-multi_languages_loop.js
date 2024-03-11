ff954b727396a4defdece54dd076c4314df8aa5e
ahmed <ahmedissaten@gmail.com>
2024-03-11 17:21:51 +0200

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  console.log(element);
+}
