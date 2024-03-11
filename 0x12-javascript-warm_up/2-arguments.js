476a06180bea13249669154dcc695faad5062b67
ahmed <ahmedissaten@gmail.com>
2024-03-11 23:08:18 +0200

    fix result

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const argsCount = process.argv.length;
 
 if (argsCount === 2) {
   console.log('No argument');
-} else if (argsCount > 1) {
+} else if (argsCount === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
