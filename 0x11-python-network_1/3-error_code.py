9e950b84b86c2bb605ffb89e529cc7b810f2e5b7
ahmed <ahmedissaten@gmail.com>
2024-03-28 14:58:50 +0200

    check error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import urllib.request
+import urllib.error
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode('utf-8')
+        print(html)
+except urllib.error.HTTPError as e:
+    print("Error code:", e.code)
