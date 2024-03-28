db4b75f376179ed3310f4a3adf3827f6c7e78055
ahmed <ahmedissaten@gmail.com>
2024-03-28 15:13:33 +0200

    request

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,11 +2,12 @@
 
 """ print X-Request-ID value """
 
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    import sys
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-with requests.get(url) as response:
-    request_id = response.headers.get('X-Request-Id')
-    print(request_id)
+    with requests.get(url) as response:
+        request_id = response.headers.get('X-Request-Id')
+        print(request_id)
