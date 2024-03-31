128e1134c6f0117858d01e2109ef97bf633e175e
ahmed <ahmedissaten@gmail.com>
2024-03-31 13:08:57 +0200

    last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    params = {'per_page': 10}
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        commits_data = response.json()
+        for commit in commits_data:
+            commit_info = commit['commit']
+            author_name = commit_info['author']['name']
+            print(f"{commit_info['tree']['sha']}: {author_name}")
