a47b28ae069c96fadb8451481cc81d8a6ed4a573
ahmed <ahmedissaten@gmail.com>
2024-03-31 12:03:27 +0200

    optimizing code

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-""" """
-
-import requests
-import sys
-
-
-def get_user_id(username, password):
-    url = 'https://api.github.com/user'
-    response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data['id']
-        print(user_id)
-    else:
-        print(None)
+""" git github user id """
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <username> <password>")
-        sys.exit(1)
+    import requests
+    import sys
+
     username = sys.argv[1]
     password = sys.argv[2]
-    get_user_id(username, password)
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+    user = response.json()
+    print(user.get('id'))
