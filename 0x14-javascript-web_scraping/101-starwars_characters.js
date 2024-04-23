d08d226cc92fa69cb18705b9e81bc9bdc8e9fb60
ahmed <ahmedissaten@gmail.com>
2024-04-23 15:23:40 +0200

    bug fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,39 @@
-#!/usr/bin/node
-// prints all characters of a Star Wars movie:
-
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
+function fetchFilmCharacters (movieId) {
+  const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+  request({ url: filmUrl, json: true }, (error, response, body) => {
+    if (error) {
+      console.error('Error fetching film:', error);
+      return;
     }
-  }
-});
+    if (response.statusCode !== 200) {
+      console.error('Failed to fetch film with status code:', response.statusCode);
+      return;
+    }
+
+    const characters = body.characters;
+    characters.forEach(characterUrl => {
+      request({ url: characterUrl, json: true }, (error, response, body) => {
+        if (error) {
+          console.error('Error fetching character:', error);
+          return;
+        }
+        if (response.statusCode !== 200) {
+          console.error('Failed to fetch character with status code:', response.statusCode);
+          return;
+        }
+        console.log(body.name);
+      });
+    });
+  });
+}
+
+// Process command-line arguments to get the movieId
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  fetchFilmCharacters(movieId);
+} else {
+  console.log('Usage: node fetchStarWarsCharacters.js <movie_id>');
+}
